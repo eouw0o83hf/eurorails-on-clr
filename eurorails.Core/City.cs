e1d5ba6aab46997f8a214fe80882c604defb485b
Nathan Landis <nathan.landis@rallyhealth.com>
2016-01-01 21:32:02 -0600

    Milepost skeleton

--- eurorails.Core/City.cs
+++ eurorails.Core/City.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace eurorails.Core
 {
-    public class City
+    public abstract class City : Milepost
     {
+        public readonly string Name;
+
+        protected City(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class SmallCity : City
+    {
+        public SmallCity(string name)
+            : base(name) { }
+    }
+
+    public class MediumCity : City
+    {
+        public MediumCity(string name)
+            : base(name) { }
+    }
+
+    public class MajorCity : City
+    {
+        public MajorCity(string name)
+            : base(name) { }
     }
 }
---
+++ eurorails.Core/Milepost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eurorails.Core
+{
+    public class Milepost
+    {
+        public ICollection<Milepost> AdjacentMileposts { get; set; }
+        public ICollection<string> AdjacentBodiesOfWater { get; set; }
+    }
+
+    public class MountainMilepost : Milepost
+    {
+    }
+
+    public class AlpineMountainMilepost : Milepost
+    {
+    }
+}
