6f4795beb1bd8afcb7da89d6b6578d49c2d48091
Nathan Landis <nathan.landis@rallyhealth.com>
2016-01-01 19:58:19 -0600

    Load cards and config

---
+++ eurorails.Core.Test/Config/DemandCardTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eurorails.Core.Config;
+using NUnit.Framework;
+
+namespace eurorails.Core.Test.Config
+{
+    [TestFixture]
+    public class DemandCardTests
+    {
+        [Test]
+        public void DemandCardConfig_ShouldMatchResourceAndCityConfigs()
+        {
+            foreach (var card in DemandCards.Values)
+            {
+                foreach (var load in card.Loads)
+                {
+                    Assert.That(CityResources.Values.ContainsKey(load.TargetCity), load.TargetCity);
+                    Assert.That(ResourceCounts.Values.ContainsKey(load.ResourceName), load.ResourceName);
+                }
+            }
+        }
+    }
+}
