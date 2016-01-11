228708dfb09fc99c626cec4acbb545c8905252ed
Nathan Landis <nathan.landis@rallyhealth.com>
2016-01-10 19:58:04 -0600

    Load mileposts and match city names

---
+++ eurorails.Core.Test/Config/MilepostsTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eurorails.Core.Config;
+using NUnit.Framework;
+
+namespace eurorails.Core.Test.Config
+{
+    [TestFixture]
+    public class MilepostsTests
+    {
+        [Test]
+        public void CityMileposts_ShouldMatchACityResource()
+        {
+            var mileposts = Mileposts.Values;
+            var cities = CityResources.Values;
+
+            foreach (var m in mileposts)
+            {
+                if (!string.IsNullOrWhiteSpace(m.Name))
+                {
+                    Assert.That(cities.ContainsKey(m.Name), "Expected " + m.Name);
+                }
+            }
+        }
+    }
+}
---
+++ eurorails.Core/Config/Mileposts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace eurorails.Core.Config
+{
+    public class Mileposts
+    {
+        public static readonly ICollection<SerializedMilepost> Values;
+
+        static Mileposts()
+        {
+            var assembly = Assembly.GetAssembly(typeof(Mileposts));
+            const string resourceName = "eurorails.Core.Config.Config_Mileposts.json";
+
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            using (var reader = new StreamReader(stream))
+            {
+                var result = reader.ReadToEnd();
+                Values = JsonConvert.DeserializeObject<ICollection<SerializedMilepost>>(result);
+            }
+        }
+    }
+}
